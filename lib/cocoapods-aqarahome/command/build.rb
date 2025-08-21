57a60ddd640ac118a2c50964ab261bdfe7297bc8
zhaoxifan <179988305@qq.com>
2025-08-21 17:22:54 +0800

    Feature: pod build方式不更新多语言

--- lib/cocoapods-aqarahome/command/build.rb
+++ lib/cocoapods-aqarahome/command/build.rb
@@ -3,19 +3,35 @@ module Pod
   class Command
     class Build < Command
       self.summary = 'A custom build command'
-      self.description = 'This is a custom build command to do something specific.'
+      self.description = 'Custom build with envs and plugin adjustments.'
+
       def initialize(argv)
         @envs = []
-
-        # 循环直到没有更多非选项参数
         while (arg = argv.shift_argument)
-          @envs << "#{arg}=0"
+          @envs << "#{arg}=1"
         end
         super
       end
 
       def run
-        system("#{@envs.join(' ')} pod install")
+        # 设置环境变量
+        @envs.each do |env|
+          key, val = env.split('=')
+          ENV[key] = val
+          UI.puts "当前环境变量 #{key}=#{val}"
+        end
+
+        podfile = Pod::Config.instance.podfile
+
+        if podfile.plugins.key?('cocoapods-aqara-localzedLoader')
+          UI.puts "跳过多语言下载和更新过程".yellow
+          podfile.plugins.delete('cocoapods-aqara-localzedLoader')
+        end
+
+        install_cmd = Pod::Command::Install.new(CLAide::ARGV.new([]))
+        install_cmd.run
+
+        podfile.plugins['cocoapods-aqara-localzedLoader'] = {}
       end
     end
   end
