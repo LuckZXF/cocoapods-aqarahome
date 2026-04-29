f5a3d57beda5891e9c1b03298da00a93c3b3b79b
zhaoxifan <179988305@qq.com>
2026-04-29 17:52:41 +0800

    解决一些依赖错误

--- lib/cocoapods-aqarahome.rb
+++ lib/cocoapods-aqarahome.rb
@@ -2,10 +2,15 @@
 
 module CocoapodsAqarahome
   require 'cocoapods-aqarahome/gem_version'
-  require 'cocoapods-aqarahome/post_install_patches'
   require 'cocoapods-aqarahome/command/Podfile_Dev'
   require 'pathname'
   require 'claide'
+
+  # 延迟加载：Podfile 解析阶段只需 DSL（dev_pods 等）；补丁逻辑在 post_install 首次执行时再加载。
+  # 使用绝对路径，避免 $LOAD_PATH 上存在多个 cocoapods-aqarahome 时误解析到旧安装目录。
+  post_install_patches_path = File.expand_path('cocoapods-aqarahome/post_install_patches.rb', __dir__)
+  autoload :PostInstallPatches, post_install_patches_path
+
   autoload :Command, 'cocoapods-aqarahome/command'
   # autoload :Podfile, 'cocoapods-aqarahome/command/Podfile_Dev'
 end
--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,5 +1,7 @@
-require 'cocoapods-aqarahome'
+# 必须先加载 build：若下面 `require 'cocoapods-aqarahome'` 抛错，PluginManager.safe_require
+# 会中止整个插件文件，导致 Pod::Command::Build 未注册，`pod build` 不可用。
 require 'cocoapods-aqarahome/command/build'
+require 'cocoapods-aqarahome'
 module Pod
   # hook
   @HooksManager = HooksManager
